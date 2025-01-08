c01576092da46d6763280f937fe7c9ccb7c43869
jason <jason.bronfeld@pm.me>
2025-01-08 15:28:42 -0500

    added deleting old file it if exists

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# if item.get('mimeType') == 'application/vnd.google-apps.folder':
 import os
 import json
 from googleapiclient.discovery import build
@@ -20,12 +19,9 @@ CLIENT_SECRETS_FILE_NAME = 'client_secret_395566799327-1r0m8lib5rv96vucqogeq7bkk
 CLIENT_SECRETS_FILE_PATH = os.path.join(BASE_PATH, CLIENT_SECRETS_FILE_NAME)
 REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 TOKEN_FILE = os.path.join(BASE_PATH, 'token.json')   # Path to store the token
-LOCATION_CODE_DICT = {
-    'CCC': '13i6LgPzykPMAx6sBgTVqP5vUNKP5MGRT', 'CDC': '1Qkr3hMgqhOVFgPiPz0c13_BNJsKoEN2l', 'ER': '1rQQN5qz8znQ7g5I_phHj2hMhuhfgXCu7',
-    'FIS': '19kntkElXjxBIAu79PsY-g97xU16RmUT0', 'NVS': '1Y_HFIgi1VMTAt3rcfEMbOHzTh9nOuXGo', 'PPC': '1CAYWhapAXKyeDn4K_oFQ-Qmo4JXOQSve',
-    'SVS': '1OG2EbO6yUsANuPom7mrEIeiss-p80ai9', 'TR': '1WwISdt8oDjJ5rFMTTea-eHkl7fDidp1A', 'WH': '1nY88WuvZHvKTtSaQukqjjOxLinXgEasK'
-}
+IMAGE_FOLDER_CODE = '1it58YP-BQlQMdTvVsTQn-E6q7gO0ac5y'
 FILE_URL_TEMPLATE = Template('https://drive.google.com/file/d/$fid/view?usp=drive_link')
+GOOGLE_NAME = 'google_urls.csv'
 
 
 def authenticate_user():
@@ -89,21 +85,19 @@ def load_credentials():
     return None
 
 
-def get_file_info(creds, root_folder_id):
+def get_file_info(svc, root_folder_id):
     # Uses the Drive API to list ALL files and folders with pagination.
     try:
-        service = build('drive', 'v3', credentials=creds)
         items = []
         page_token = None
-
         while True:
-            results = service.files().list(
+            results = svc.files().list(
                 pageSize=1000,  # Increase page size for efficiency
                 fields="nextPageToken, files(id, name, mimeType)",  # Include mimeType
                 pageToken=page_token,
                 q=f"'{root_folder_id}' in parents"
             ).execute()
-            items.extend(results.get('files', []))
+            items += results.get('files', [])
             page_token = results.get('nextPageToken')
             if not page_token:
                 break
@@ -121,14 +115,70 @@ def get_file_info(creds, root_folder_id):
         return None
 
 
-def upload_file(creds, filepath: Path):
+def upload_file(svc, filepath: Path):
 
     try:
-        service = build('drive', 'v3', credentials=creds)
         file_metadata = {'name': filepath.stem + filepath.suffix}
         media = MediaFileUpload(filepath, mimetype='text/csv')
-        result = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+        result = svc.files().create(body=file_metadata, media_body=media, fields='id').execute()
         return result.get("id")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def list_folders(svc, parent_id='root', folder_list=None):
+    if folder_list is None:
+        print('Building folder list')
+        folder_list = []
+    try:
+        query = f"mimeType = 'application/vnd.google-apps.folder' and trashed = false and '{parent_id}' in parents"
+        results = svc.files().list(q=query, spaces='drive', fields='nextPageToken, files(id, name)').execute()
+        items = results.get('files', [])
+        for item in items:
+            folder_list.append({'id': item['id'], 'name': item['name'], 'parent': parent_id})
+            list_folders(svc, item['id'], folder_list)  # Recursive call
+        return folder_list
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+def list_files(svc, folder_list):
+    print('Building file dictionary')
+    file_list = []
+    try:
+        for folder_id in ['root'] + [f['id'] for f in folder_list]:
+            items = []
+            page_token = None
+            while True:
+                results = svc.files().list(
+                    pageSize=1000,  # Increase page size for efficiency
+                    fields="nextPageToken, files(id, name, mimeType)",  # Include mimeType
+                    pageToken=page_token,
+                    q=f"'{folder_id}' in parents and trashed = false"
+                ).execute()
+                items += results.get('files', [])
+                page_token = results.get('nextPageToken')
+                if not page_token:
+                    break
+            file_list += items
+
+        name_dict = {f['name']: f for f in file_list}
+        return name_dict
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+def find_files(f_name: str, f_dict: dict):
+    return [f_dict[key] for key in f_dict.keys() if f_name in key]
+
+
+def delete_file(svc, fid):
+    try:
+        svc.files().delete(fileId=fid).execute()
+        print('fid has been successfully deleted.')
     except Exception as e:
         print(f"An error occurred: {e}")
 
@@ -139,12 +189,24 @@ if __name__ == '__main__':
         credentials = authenticate_user()
 
     if credentials:
+        service = build('drive', 'v3', credentials=credentials)
+        f_list = list_folders(service)
+        file_dict = list_files(service, f_list)
+        values = find_files(GOOGLE_NAME, file_dict)
+        if len(values):
+            for v in values:
+                print(f'{v['name']} found.')
+                delete_file(service, v['id'])
+
         output_frame = pd.DataFrame()
-        for loc_key in LOCATION_CODE_DICT.keys():
-            speed_folders = get_file_info(credentials, LOCATION_CODE_DICT[loc_key])
-            speed_dict = {speed['name']: speed['id'] for speed in speed_folders}
-            for speed_key in speed_dict.keys():
-                files = get_file_info(credentials, speed_dict[speed_key])
+        location_folders = [f for f in get_file_info(service, IMAGE_FOLDER_CODE) if f['mimeType'] == 'application/vnd.google-apps.folder']
+        for loc_folder in location_folders:
+            code = loc_folder['name'].upper()
+            print(f'{code}')
+            speed_folders = [f for f in get_file_info(service, loc_folder['id']) if f['mimeType'] == 'application/vnd.google-apps.folder']
+            for speed_folder in speed_folders:
+                files = get_file_info(service, speed_folder['id'])
+                print(f'        {code} {speed_folder['name']} {len(files)}')
                 frame = pd.DataFrame(columns=['date', 'id'])
                 for file in files:
                     fields = file['name'].split()
@@ -153,14 +215,13 @@ if __name__ == '__main__':
                 frame.sort_values(by=['date'], inplace=True)
                 frame = frame.transpose()
                 frame = frame.drop(index=frame.index[0], axis=0).reset_index(drop=True)
-                frame.insert(0, 'code', loc_key)
-                frame.insert(0, 'speed', int(speed_key))
+                frame.insert(0, 'code', code)
+                frame.insert(0, 'speed', int(speed_folder['name']))
                 output_frame = pd.concat([output_frame, frame], axis=0)
-                print(f'{speed_key} {loc_key}')
                 del frame
         output_frame.columns = ['speed', 'code'] + [i+1 for i in range(len(output_frame.columns) - 2)]
         output_frame = output_frame.sort_values(by=['code', 'speed']).reset_index(drop=True)
-        print_file_exists(write_df(output_frame, Path(BASE_PATH).joinpath('google_urls.csv')))
-        file_id = upload_file(credentials, Path(BASE_PATH).joinpath('google_urls.csv'))
+        print_file_exists(write_df(output_frame, Path(BASE_PATH).joinpath(GOOGLE_NAME)))
+        file_id = upload_file(service, Path(BASE_PATH).joinpath(GOOGLE_NAME))
     else:
         print("Authentication failed. Cannot access Google Drive.")
